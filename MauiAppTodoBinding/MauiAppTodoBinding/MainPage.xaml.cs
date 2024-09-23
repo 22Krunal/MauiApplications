c1515db67a7fdea25080a324df0376de17be18f6
22Krunal <202001442@daiict.ac.in>
2024-09-23 18:57:01 +0530

    MauiTodo app with DataBinding

--- MauiAppTodoBinding/MauiAppTodoBinding/MainPage.xaml.cs
+++ MauiAppTodoBinding/MauiAppTodoBinding/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using MauiAppTodoBinding.Models;
 using MauiAppTodoBinding.Data;
+using System.Collections.ObjectModel;
 
 namespace MauiAppTodoBinding
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<TodoItem> Todos { get; set; } = new();
+
         string _todoListData = string.Empty;
         readonly Database _database;
 
@@ -24,8 +27,9 @@ namespace MauiAppTodoBinding
             {
                 _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
                 //todos.Add(todo);
+                Todos.Add(todo);
             }
-            TodosLabel.Text = _todoListData;
+            //TodosLabel.Text = _todoListData;
         }
         private async void Button_Clicked(object sender, EventArgs e)
         {
@@ -40,9 +44,9 @@ namespace MauiAppTodoBinding
             if (inserted != 0)
             {
                 _todoListData += $"{todo.Title} - {todo.Due:f}{Environment.NewLine}";
-                //Todos.Add(todo);
+                Todos.Add(todo);
 
-                TodosLabel.Text = _todoListData;
+                //TodosLabel.Text = _todoListData;
 
                 TodotitleEntry.Text = String.Empty;
                 DueDatepicker.Date = DateTime.Now;
