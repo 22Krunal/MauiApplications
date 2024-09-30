e4c20a1eb6fae4873f2123a0aeb16f9ea9a08bbe
22Krunal <202001442@daiict.ac.in>
2024-09-30 22:28:53 +0530

    Chep 7 Completed

---
+++ MauiStockTake/src/Presentation/MauiStockTake.UI/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiStockTake.UI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        Routing.RegisterRoute("login", typeof(LoginPage));
+        Routing.RegisterRoute("productdetails", typeof(ProductPage));
+    }
+}
